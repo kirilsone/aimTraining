c784c518a03e429439b0bafc33e3b896d1498275
KIR <kirilsone@gmail.com>
2022-03-09 18:15:59 +0200

    42

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
+let score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
 
 function startGame() {
 	$game.style.backgroundColor = 'grey';
@@ -10,6 +13,38 @@ function startGame() {
 	renderBox()
 }
 
-function renderBox() {
-
+function handleBoxClick(event) {
+	if (event.target.dataset.box) {
+		score++
+		renderBox()
+	}
 }
+
+function renderBox() {
+	$game.innerHTML = ''
+	let box = document.createElement('div')
+
+	box.style.height = box.style.width = '50px'
+	box.style.position = 'absolute'
+	box.style.backgroundColor = '#000'
+	box.style.top = '50px'
+	box.style.left = '70px'
+	box.style.cursor = 'pointer'
+	box.setAttribute('data-box', 'true')
+
+	$game.insertAdjacentElement('afterbegin', box)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
